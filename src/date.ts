2d918399600bb13cd1f01023a979edd766b54be4
MC_XiaoHei <xor7xiaohei@gmail.com>
2026-05-19 17:57:40 +0800

    feat: add [new] tag for activities in 1 month

--- src/date.ts
+++ src/date.ts
@@ -1,5 +1,15 @@
 export function localizeDate(date: Date) {
   return date
-    .toLocaleDateString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit' })
-    .replace(/\//g, '.');
+    .toLocaleDateString("zh-CN", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replace(/\//g, ".");
+}
+
+export function isWithinDays(date: Date, days: number): boolean {
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  return diff >= 0 && diff <= days * 24 * 60 * 60 * 1000;
 }
